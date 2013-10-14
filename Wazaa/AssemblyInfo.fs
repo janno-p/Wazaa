3075ad0416dbcdccef2d5c75411bc2b0ae8ba84a
Janno Põldma <janno.poldma@gmail.com>
2013-10-14 12:53:22 +0300

    Initial F#/Gtk application.

---
+++ Wazaa/AssemblyInfo.fs
@@ -0,0 +1,22 @@
+module Wazaa.AssemblyInfo
+open System.Reflection
+open System.Runtime.CompilerServices
+
+
+[<assembly: AssemblyTitle("Wazaa")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("janno")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
+
