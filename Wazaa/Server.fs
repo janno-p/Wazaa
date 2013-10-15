ff5d5da5b036a89c4f9685655c5357e96cd0bf9b
Janno Põldma <janno.poldma@gmail.com>
2013-10-15 17:56:15 +0300

    Parsing HTTP request path and query parameters.

--- Wazaa/Server.fs
+++ Wazaa/Server.fs
@@ -5,12 +5,44 @@ namespace Wazaa
         open System.Net
         open System.Net.Sockets
         open System.Text
+        open System.Text.RegularExpressions
 
         let headers = Encoding.ASCII.GetBytes("HTTP/1.0 200 OK\r\nContent-Type: text/plain; charset=UTF-8\r\nContent-Length: 1\r\nServer: Wazaa/0.0.1\r\n\r\n")
         let content = Encoding.ASCII.GetBytes("0")
 
+        let pattern = @"^(?<method>GET|POST)\s+\/?(?<path>.*?)(\s+HTTP\/1\.[01])$"
+
+        let ParsePair (c:char) (pair:string) =
+            let breakAt = pair.IndexOf(c)
+            if breakAt < 0 then
+                (WebUtility.UrlDecode(pair), "")
+            else
+                (WebUtility.UrlDecode(pair.Substring(0, breakAt)), WebUtility.UrlDecode(pair.Substring(breakAt + 1)))
+
+        let ParseQueryParams (query:string) =
+            if String.IsNullOrEmpty(query) then
+                Map.empty
+            else
+                query.Split('&') |> Seq.map (ParsePair '=') |> Map.ofSeq
+
+        let (|Path|_|) request =
+            let m = Regex.Match(request, pattern, RegexOptions.IgnoreCase)
+            if m.Success then
+                let (path, query) = ParsePair '?' m.Groups.["path"].Value
+                let queryParams = ParseQueryParams query
+                Some (m.Groups.["method"].Value.ToUpper(), path, queryParams)
+            else None
+
         let serveClient (client:TcpClient) = async {
             use stream = client.GetStream()
+            use reader = new StreamReader(stream)
+            let request = reader.ReadLine()
+            printfn "%s" request
+            match request with
+                | Path ("GET", "getfile", query) -> printfn "TODO: Get File: %O" query
+                | Path ("GET", "searchfile", query) -> printfn "TODO: Search File: %O" query
+                | Path ("POST", "foundfile", query) -> printfn "TODO: Found File: %O" query
+                | _ -> printfn "not ok"
             do! stream.AsyncWrite(headers)
             do! stream.AsyncWrite(content)
             stream.Close()
