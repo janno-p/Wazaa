5403f9f5114b237260a3ddd8466bcf3c9969dbfb
Janno Põldma <janno.poldma@gmail.com>
2013-10-20 18:52:06 +0300

    Fixed solution / project files.

--- Wazaa.Console/Main.fs
+++ Wazaa.Console/Main.fs
@@ -3,7 +3,7 @@ module Wazaa.Console.Main
 open System
 open System.Configuration
 open System.Threading
-open Wazaa.Server
+open Wazaa
 
 [<EntryPoint>]
 let main args = 
@@ -13,7 +13,7 @@ let main args =
     if not (Int32.TryParse(ConfigurationManager.AppSettings.["port"], &port)) then
         port <- 0
 
-    let server = HttpServer(host, port)
+    let server = Server.HttpServer(host, port)
     printfn "Press [CTRL+C] to shut down the server!"
 
     Console.CancelKeyPress.AddHandler (fun o e ->
@@ -21,7 +21,7 @@ let main args =
         printfn "Server shut down."
     )
 
-    server |> RunServerAsync |> Async.RunSynchronously
+    server |> Server.RunServerAsync |> Async.RunSynchronously
     server.Stop()
 
     0
